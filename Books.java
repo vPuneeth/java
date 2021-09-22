3a71cfd1157563a1fb0d477d0f3b5a57330d9284
Puneeth <vpuneeth.xworkz@gmail.com>
2021-09-22 22:32:04 +0530

    a

---
+++ Books.java
@@ -0,0 +1,22 @@
+class Books{
+
+static String bookName="view";
+static String authorName="dhananjay";
+  static int releaseDate=2019;
+
+static boolean isItReleased=true;
+
+
+
+public static void main(String a[]){
+
+  
+ 
+System.out.println(bookName);
+System.out.println( authorName);
+System.out.println(releaseDate);
+
+System.out.println(isItReleased);
+
+   }
+}
